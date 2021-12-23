2f4c21b2b1a58b086435668474ec828e9b44d0f6
Vitor Neves <vitornv@dcomp.ufs.br>
2021-12-23 17:34:41 -0300

    feat: add validation of  duplicate name in update branch service

--- src/app/services/branches/update/index.ts
+++ src/app/services/branches/update/index.ts
@@ -1,12 +1,18 @@
 import { UpdateBranch } from '../../../../domain/useCases/branches/UpdateBranch';
+import { ListBranchesRepository } from '../../../contracts/db/branches/ListBranches';
 import { UpdateBranchRepository } from '../../../contracts/db/branches/UpdateBranch';
 
 export class UpdateBranchService implements UpdateBranch {
   constructor(
+    private readonly findBranchByName: ListBranchesRepository,
     private readonly updateBranchRepository: UpdateBranchRepository,
   ) {}
   async update(id: string, name: string): Promise<boolean> {
-    await this.updateBranchRepository.update(id, name);
-    return true;
+    const nameAlreadyExists = await this.findBranchByName.list(null, name);
+    if (!nameAlreadyExists.length) {
+      await this.updateBranchRepository.update(id, name);
+      return true;
+    }
+    return false;
   }
 }
--- src/main/factories/services/branches/UpdateBranch.ts
+++ src/main/factories/services/branches/UpdateBranch.ts
@@ -2,6 +2,6 @@ import { UpdateBranchService } from '../../../../app/services/branches/update';
 import { PgBranchesRepository } from '../../../../infra/database/repositories/branches';
 
 export const makeUpdateBranch = () => {
-  const updateBranchRepository = new PgBranchesRepository();
-  return new UpdateBranchService(updateBranchRepository);
+  const branchesRepository = new PgBranchesRepository();
+  return new UpdateBranchService(branchesRepository, branchesRepository);
 };
