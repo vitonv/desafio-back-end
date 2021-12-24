5f69893cd38ab7d4fc2b1f5a5f7ae93f012dab67
Vitor Neves <vitornv@dcomp.ufs.br>
2021-12-24 10:47:18 -0300

    test: add tests for createEmployeeService

--- src/app/services/employees/create/CreateEmployeeService.spec.ts
+++ src/app/services/employees/create/CreateEmployeeService.spec.ts
@@ -39,6 +39,54 @@ const makeSut = () => {
   };
 };
 
+const makeFakeCreate = {
+  name: 'any_name',
+  branch_name: 'branch_name',
+};
 describe('CreateEmployee Service', () => {
-  it('', () => {});
+  it('Should call findBranchRepository with correct value', async () => {
+    const { sut, findBranchRepositorySpy } = makeSut();
+    const listSpy = jest.spyOn(findBranchRepositorySpy, 'list');
+    await sut.create(makeFakeCreate);
+    expect(listSpy).toHaveBeenCalledWith(null, 'branch_name');
+  });
+  it('Should throw if findBranchRepository throws', async () => {
+    const { sut, findBranchRepositorySpy } = makeSut();
+    jest
+      .spyOn(findBranchRepositorySpy, 'list')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const error = sut.create(makeFakeCreate);
+    await expect(error).rejects.toThrow();
+  });
+  it('Should return false if findBranchRepository returns falsy', async () => {
+    const { sut, findBranchRepositorySpy } = makeSut();
+    jest
+      .spyOn(findBranchRepositorySpy, 'list')
+      .mockReturnValueOnce(Promise.resolve([]));
+    const result = await sut.create(makeFakeCreate);
+    expect(result).toBe(false);
+  });
+  it('Should call createEmployeeRepository with correct values', async () => {
+    const { sut, createEmployeeRepositorySpy } = makeSut();
+    const createSpy = jest.spyOn(createEmployeeRepositorySpy, 'create');
+    await sut.create(makeFakeCreate);
+    expect(createSpy).toHaveBeenCalledWith({
+      name: makeFakeCreate.name,
+      branch_id: 'branch_id',
+    });
+  });
+  it('Should throw if createEmployeeRepository throws', async () => {
+    const { sut, createEmployeeRepositorySpy } = makeSut();
+    jest
+      .spyOn(createEmployeeRepositorySpy, 'create')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const error = sut.create(makeFakeCreate);
+    await expect(error).rejects.toThrow();
+  });
+  it('Should return true on success', async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.create(makeFakeCreate);
+    expect(result).toBe(true);
+  });
 });
