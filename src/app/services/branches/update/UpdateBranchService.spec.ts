8444d11d3f9f2c8928cf79209a70cc4835dd838a
Vitor Neves <vitornv@dcomp.ufs.br>
2021-12-24 11:20:11 -0300

    test: add tests for updateBranchService

--- src/app/services/branches/update/UpdateBranchService.spec.ts
+++ src/app/services/branches/update/UpdateBranchService.spec.ts
@@ -29,16 +29,45 @@ const makeSut = () => {
   return {
     sut,
     updateBranchRepositorySpy,
+    listBranchesRepositorySpy,
   };
 };
 describe('UpdateBranch Service', () => {
-  it('Should call updateBranchService with correct values', async () => {
+  it('Should call listBranchesRepository with correct values', async () => {
+    const { sut, listBranchesRepositorySpy } = makeSut();
+    const listSpy = jest.spyOn(listBranchesRepositorySpy, 'list');
+    await sut.update('any_id', 'new_name');
+    expect(listSpy).toHaveBeenCalledWith(null, 'new_name');
+  });
+  it('Should throw if listBranchesRepository throws', async () => {
+    const { sut, listBranchesRepositorySpy } = makeSut();
+    jest
+      .spyOn(listBranchesRepositorySpy, 'list')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+    const error = sut.update('any_id', 'new_name');
+    expect(error).rejects.toThrow();
+  });
+  it('Should throw if listBranchesRepository throws', async () => {
+    const { sut, listBranchesRepositorySpy } = makeSut();
+    jest.spyOn(listBranchesRepositorySpy, 'list').mockReturnValueOnce(
+      Promise.resolve([
+        {
+          id: 'any_id',
+          name: 'any_name',
+          employees: 0,
+        },
+      ]),
+    );
+    const result = await sut.update('any_id', 'new_name');
+    expect(result).toBe(false);
+  });
+  it('Should call updateBranchRepository with correct values', async () => {
     const { sut, updateBranchRepositorySpy } = makeSut();
     const updateSpy = jest.spyOn(updateBranchRepositorySpy, 'update');
     await sut.update('any_id', 'new_name');
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'new_name');
   });
-  it('Should throw if updateBranchService throws', async () => {
+  it('Should throw if updateBranchRepository throws', async () => {
     const { sut, updateBranchRepositorySpy } = makeSut();
     jest
       .spyOn(updateBranchRepositorySpy, 'update')
