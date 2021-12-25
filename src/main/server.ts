3d9ed53253ba39178a73bc3dd7a95d3b4a9d0958
Vitor Neves <vitornv@dcomp.ufs.br>
2021-12-25 13:13:48 -0300

    config: fix heroku config for deploy

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,12 @@
 import { PgConnection } from '../infra/database/helpers/connection';
 import { app } from './config/app';
+import 'dotenv/config';
 
 PgConnection.getInstance()
   .connect()
   .then(async () => {
     const { app } = await import('./config/app');
-    app.listen(5050, () => {
+    app.listen(process.env.PORT || 5050, () => {
       console.log('Server started at http://localhost:5050/api/docs');
     });
   });
