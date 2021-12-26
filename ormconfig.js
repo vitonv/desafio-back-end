b096199133522f928ba846e146cae3e4c97b094c
Vitor Neves <vitornv@dcomp.ufs.br>
2021-12-25 23:11:12 -0300

    config: ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+require('reflect-metadata')
+require('dotenv/config')
+
+module.exports = [{
+  name: 'default',
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  syncrhonize: false,
+  extra: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  entities: ["./dist/infra/database/entities/**/*.js"],
+  migrations: ["./dist/infra/database/migrations/*.js"],
+  cli: {
+    migrationsDir: "./src/infra/database/migrations",
+  },
+}]
